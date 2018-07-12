aaa6af33795b84b098a4749ad1750c38d927a8bd
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-07-11 21:12:04 -0300

    Setting production database and production seeds

--- config/database.rb
+++ config/database.rb
@@ -21,12 +21,13 @@ configure :development do
 end
 
 configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/faqbot_production')
   set :database, {
-    adapter: 'postgresql',
-    enconding: 'utf8',
-    database: 'faqbot_production',
-    pool: 5,
-    username: 'postgres',
-    host: 'postgres'
+    adapter:   db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    host:      db.host,
+    username:  db.user,
+    password:  db.password,
+    database:  db.path[1..-1],
+    enconding: 'utf8'
   }
 end
