afa576412bd73e144cc1a7c3ad8a986dc28424b3
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-07-13 15:29:41 -0300

    Ajusting quote service response format

--- app/services/quote_service.rb
+++ app/services/quote_service.rb
@@ -2,6 +2,8 @@ class QuoteService
   def self.call
     quotes = JSON.parse(File.read('app/assets/quotes.json'))
     sample = quotes['citacao'].sample
-    "_#{sample['quote']}_ \n\n >Autor: _#{sample['author']}_"
+    
+    response = "_#{sample['quote']}_ \n\n"
+    response += ">Autor: _#{sample['author']}_"
   end
 end
