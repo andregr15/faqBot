3b04667f939984e79e453da59bfcab0df1d7ea51
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2018-07-13 15:26:06 -0300

    Ajusting help service for quote service

--- app/services/help_service.rb
+++ app/services/help_service.rb
@@ -13,5 +13,7 @@ class HelpService
     response += ">Lista as perguntas e respostas com aquela hashtag\n\n"
     response += "*Perguntas e Respostas*\n"
     response += ">Mostra a lista de perguntas e respostas\n\n"
+    response += "*Citações*\n"
+    response += ">Mostra citações inspiradoras"
   end
 end
